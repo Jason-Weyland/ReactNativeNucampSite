e559c9216dac830cff74e7f46c84fdc913d4136e
Jason Weyland <jason.weyland13@gmail.com>
2020-07-08 11:40:53 -0400

    Setting up Redux

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Main from "./components/MainComponent";
 
-console.disableyellowbox = true;
 export default function App() {
-  return <Main />;
+  console.disableyellowbox = true;
+  return (
+    <Main />
+  );
 }
